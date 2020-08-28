91b04d507c1bed7586f5af4f99cc59dc593f4eaf
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2020-08-28 08:55:29 -0600

    Add project files.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("TrappingErrors")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("TrappingErrors")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("a7b98050-c6ad-46f9-a08f-623811a5fc5f")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ TrappingErrors.vb
@@ -0,0 +1,41 @@
+﻿Option Strict On
+Option Explicit On
+Option Compare Binary
+
+Module TrappingErrors
+
+    Sub Main()
+        Dim userInput As String
+        Dim userNumber As Integer
+
+
+        'While userInput <> "Q" And userInput <> "q"
+        Do
+
+            Console.WriteLine("Type a number and press enter. Enter Q to quit.")
+            userInput$ = Console.ReadLine()
+
+            Try
+                'try to convert user input from string to integer
+                userNumber% = CInt(userInput$)
+                Console.WriteLine(userInput & " + 5 is:")
+                Console.WriteLine(userNumber% + 5)
+            Catch ex As InvalidCastException
+                Console.WriteLine("I asked for a number... You entered: " & userInput)
+            Catch ex As OverflowException
+                Console.WriteLine(userInput & " is too big!!")
+            Catch ex As Exception
+                Console.WriteLine("You broke it!!!")
+                Console.WriteLine(ex.Message)
+            Finally
+                'always runs
+            End Try
+
+        Loop Until userInput$ = "Q" Or userInput$ = "q"
+        'End While
+
+
+        Console.ReadLine()
+    End Sub
+
+End Module
